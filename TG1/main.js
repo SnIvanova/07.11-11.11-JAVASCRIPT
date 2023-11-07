a5eca753d0cb8c78f61df8979f1c98d4460143f6
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-07 12:31:17 +0100

    Update main.js

--- TG1/main.js
+++ TG1/main.js
@@ -14,19 +14,21 @@
  }
 
 //let name= obj1.name, let lastname= obj1.lastname same as:
-let{age}= obj1
-console.log(age)
+let{age}= obj1; console.log(age);
 
 
- let obj2 = Object.assign ({}, obj1	)
- let obj3 = {
-    ...obj1,
-    citty: ' ',
-    age: 20,
- }
+
+let obj2 = Object.assign ({}, obj1)
+ let obj3 = {...obj1, citty: ' ', age: 20, }
 
 
- function func1(...args) {
-    console.log(args.length > 1)
- }
- func1(1,3,4,2)
+ function func1(...args) { console.log(args.length > 1); } func1(1,3,4,2);
+
+ let [ , , z] =arr1; let x = arr1[1]; console.log (x,z);
+
+ let str = `ciao sono ${obj1.name}`;
+ console.log(str); 
+
+ let metodarr = [4,3,5,6,7,8,9,10,11,12];
+ metodarr = [...metodarr, obj3.age];
+  console.log(metodarr);
